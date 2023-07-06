3c8c722282e809f8344ea74e8b9d98aaad451a68
Jair Torres <jairalex0306.jatc@gmail.com>
2023-07-05 23:42:55 -0500

    Add show option in context menu

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,28 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { ExtensionContext, commands, languages, window, WebviewPanel } from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "JT-View-Exports-SVG" is now active!');
+// this method is called when your extension is activated
+export function activate(context: ExtensionContext) {
+  console.log(
+    'Congratulations, your extension "JT-View-Exports-SVG" is now active!'
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('JT-View-Exports-SVG.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from View Exports SVG!');
-	});
+  // Registro del menú contextual
 
-	context.subscriptions.push(disposable);
+  const webviewPanels: Map<string, WebviewPanel> = new Map();
+  const regexp: RegExp = /.+\.svg$/i;
+
+  context.subscriptions.push(
+    commands.registerCommand(
+      "JT-View-Exports-SVG.showMenu",
+      (item: any, items: any[]) => {
+        const selectedFiles: any[] = [];
+        const selectedFolders: any[] = [];
+        console.log({item, items});
+        window.showInformationMessage("Hello, View Exports SVG!");
+      }
+    )
+  );
 }
 
 // This method is called when your extension is deactivated
