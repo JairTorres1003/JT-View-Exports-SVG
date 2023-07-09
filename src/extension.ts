8c84860f48e47b02b814234dc37d9610344884ae
Jair Torres <jairalex0306.jatc@gmail.com>
2023-07-08 23:28:04 -0500

    update

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,11 @@
 import * as vscode from "vscode";
 
-// const createPanel = async (
-//   context: ExtensionContext
-// ): Promise<WebviewPanel> => {
-//   const panel = window.createWebviewPanel(
-//     "viewExportsSVG",
-//     "View Exports SVG",
-//     { viewColumn: window.activeTextEditor?.viewColumn || 1 }
-//   );
-
-//   return panel;
-// };
-
+/**
+ * Run the command and create or show the webview panel.
+ * @param context The extension context.
+ * @param item The selected item.
+ * @param items The list of items.
+ */
 const runCommand = async (
   context: vscode.ExtensionContext,
   item: any,
@@ -20,10 +14,14 @@ const runCommand = async (
   const selectedFiles: any[] = [];
   const selectedFolders: any[] = [];
 
+  // Create or show the webview panel
   ViewExportsSVGPanel.createOrShow(context.extensionUri);
 };
 
-// this method is called when your extension is activated
+/**
+ * This method is called when your extension is activated.
+ * @param context The extension context.
+ */
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand(
@@ -33,26 +31,34 @@ export function activate(context: vscode.ExtensionContext) {
   );
 }
 
-// This method is called when your extension is deactivated
+/**
+ * This method is called when your extension is deactivated.
+ */
 export function deactivate() {}
 
+/**
+ * Get the webview options.
+ * @param extensionUri The extension URI.
+ * @returns The webview options.
+ */
 function getWebviewOptions(extensionUri: vscode.Uri): vscode.WebviewOptions {
   return {
-		// Enable javascript in the webview
-		enableScripts: true,
-
-		// And restrict the webview to only loading content from our extension's `assets` directory.
-		localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'assets')]
-	};
+    // Enable JavaScript in the webview
+    enableScripts: true,
+    // Restrict the webview to only load content from the extension's `assets` and `webview` directories
+    localResourceRoots: [
+      vscode.Uri.joinPath(extensionUri, "src", "assets"),
+      vscode.Uri.joinPath(extensionUri, "src", "webview"),
+    ],
+  };
 }
 
-
 /**
- * Manages view exports svg webview panels
+ * Webview panel for displaying SVG exports.
  */
 class ViewExportsSVGPanel {
   /**
-   * Track the currently panel. Only allow a single panel to exist at a time.
+   * Track the currently active panel. Only allow a single panel to exist at a time.
    */
   public static currentPanel: ViewExportsSVGPanel | undefined;
 
@@ -62,23 +68,39 @@ class ViewExportsSVGPanel {
   private readonly _extensionUri: vscode.Uri;
   private _disposables: vscode.Disposable[] = [];
 
+  /**
+   * Create a new instance of the ViewExportsSVGPanel class.
+   * @param panel The webview panel.
+   * @param extensionUri The extension URI.
+   */
   private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
     this._panel = panel;
-		this._extensionUri = extensionUri;
-	}
+    this._extensionUri = extensionUri;
 
+    // Set the webview's initial HTML content
+    this._update();
+
+    // Listen for when the panel is disposed
+    // This happens when the user closes the panel or when the panel is closed programmatically
+    this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+  }
+
+  /**
+   * Create a new panel or show an existing one.
+   * @param extensionUri The extension URI.
+   */
   public static createOrShow(extensionUri: vscode.Uri) {
     const column = vscode.window.activeTextEditor
       ? vscode.window.activeTextEditor.viewColumn
       : undefined;
 
-    // If we already have a panel, show it.
+    // If we already have a panel, show it
     if (ViewExportsSVGPanel.currentPanel) {
       ViewExportsSVGPanel.currentPanel._panel.reveal(column);
       return;
     }
 
-    // Otherwise, create a new panel.
+    // Otherwise, create a new panel
     const panel = vscode.window.createWebviewPanel(
       ViewExportsSVGPanel.viewType,
       "View Exports SVG",
@@ -86,6 +108,45 @@ class ViewExportsSVGPanel {
       getWebviewOptions(extensionUri)
     );
 
-    ViewExportsSVGPanel.currentPanel = new ViewExportsSVGPanel(panel, extensionUri);
+    ViewExportsSVGPanel.currentPanel = new ViewExportsSVGPanel(
+      panel,
+      extensionUri
+    );
+  }
+
+  /**
+   * Update the webview panel's content.
+   */
+  private _update() {
+    this._panel.webview.html = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>View Exports SVG</title>
+      </head>
+      <body>
+        <div id="root">Hi, View Exports SVG!</div>
+      </body>
+      </html>
+    `;
+  }
+
+  /**
+   * Dispose of the panel and clean up resources.
+   */
+  public dispose() {
+    ViewExportsSVGPanel.currentPanel = undefined;
+
+    // Clean up resources
+    this._panel.dispose();
+
+    while (this._disposables.length) {
+      const disposable = this._disposables.pop();
+      if (disposable) {
+        disposable.dispose();
+      }
+    }
   }
 }
