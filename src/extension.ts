02901516ddede48407a461b4c47810316f597085
Jair Torres <83931760+JairTorres1003@users.noreply.github.com>
2023-07-14 23:29:48 +0000

    Add Loader

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,4 @@
-import { ExtensionContext, commands } from "vscode";
+import { ExtensionContext, commands, window, ProgressLocation } from "vscode";
 import { ViewExportsSVGPanel } from "./panels/ViewExportsSVGPanel";
 import { extractSVGComponentExports } from "./utilities/exportParser";
 
@@ -11,39 +11,53 @@ import { extractSVGComponentExports } from "./utilities/exportParser";
 const runCommand = async (context: ExtensionContext, item: any, items: any[]) => {
   const selectedFiles: any[] = [];
   const REGEX_FILE = /\.(js|jsx|ts|tsx)$/i;
+  const progressOptions = {
+    location: ProgressLocation.Notification,
+    title: "Extracting SVG exports...",
+    cancellable: false,
+  };
 
-  // Check if there are multiple items
-  if (items && items?.length > 1) {
-    items.forEach((file) => {
-      // Check if the item is a file with a supported extension
-      if (REGEX_FILE.test(file.fsPath.slice(-4)) && file.scheme === "file") {
-        selectedFiles.push(file.fsPath);
+  // Show loader message
+  const progress: any = await window.withProgress(progressOptions, async () => {
+    // Check if there are multiple items
+    if (items && items?.length > 1) {
+      items.forEach((file) => {
+        // Check if the item is a file with a supported extension
+        if (REGEX_FILE.test(file.fsPath.slice(-4)) && file.scheme === "file") {
+          selectedFiles.push(file.fsPath);
+        }
+      });
+    } else {
+      // Check if the selected item is a file with a supported extension
+      if (REGEX_FILE.test(item.fsPath.slice(-4)) && item.scheme === "file") {
+        selectedFiles.push(item.fsPath);
       }
-    });
-  } else {
-    // Check if the selected item is a file with a supported extension
-    if (REGEX_FILE.test(item.fsPath.slice(-4)) && item.scheme === "file") {
-      selectedFiles.push(item.fsPath);
     }
-  }
 
-  // Extract the exports from selected files
-  const exportsData = await Promise.all(
-    selectedFiles.map(async (filePath) => {
-      try {
-        const svgExports = await extractSVGComponentExports(filePath);
-        return { filePath, svgExports };
-      } catch (error) {
-        console.error(`Error parsing file ${filePath}: ${error}`);
-        return { filePath, svgExports: [] };
-      }
-    })
-  ).finally(() => console.log("FINALIZO"));
+    // Extract the exports from selected files
+    const exportsData = await Promise.all(
+      selectedFiles.map(async (filePath) => {
+        try {
+          const svgExports = await extractSVGComponentExports(filePath);
+          return { filePath, svgExports };
+        } catch (error) {
+          console.error(`Error parsing file ${filePath}: ${error}`);
+          return { filePath, svgExports: [] };
+        }
+      })
+    ).finally(() => console.log("FINALIZO"));
 
-  console.log(exportsData);
+    console.log(exportsData);
 
-  // Create or show the webview panel
-  ViewExportsSVGPanel.render(context.extensionUri);
+    // Create or show the webview panel
+    ViewExportsSVGPanel.render(context.extensionUri);
+
+    return exportsData;
+  });
+
+  // Hide the loader message
+  progress.report({ increment: 100 });
+  progress.dispose();
 };
 
 /**
