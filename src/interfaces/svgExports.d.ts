32096642aac4d7dc716563aa9d44eb95f0523362
Jair Torres <83931760+JairTorres1003@users.noreply.github.com>
2023-07-18 21:39:27 +0000

    Add news interfaces

---
+++ src/interfaces/svgExports.d.ts
@@ -0,0 +1,67 @@
+import { JSXIdentifier } from "@babel/types";
+
+/**
+ * Represents the exported SVG components from a file.
+ */
+export interface SvgExport {
+  /**
+   * The file object containing the absolute and relative paths of the SVG file.
+   */
+  file: {
+    /**
+     * The absolute path of the SVG file.
+     */
+    absolutePath: string;
+    /**
+     * The relative path of the SVG file.
+     */
+    relativePath: string;
+  };
+  /**
+   * An array of SvgComponent objects representing the exported SVG components.
+   */
+  svgExports: SvgComponent[];
+}
+
+/**
+ * Represents an SVG component.
+ */
+export interface SvgComponent {
+  /**
+   * An object representing the SVG component details.
+   */
+  component: SvgComponentDetails | undefined;
+  /**
+   * The name of the SVG component.
+   */
+  name: string;
+  /**
+   * The location of the SVG component in the file.
+   */
+  location: { column: number; line: number } | undefined;
+  /**
+   * The type of export for the SVG component.
+   */
+  typeExport: "function" | "variable";
+}
+
+/**
+ * Represents the details of an SVG component.
+ */
+export interface SvgComponentDetails {
+  /**
+   * The name of the component.
+   */
+  componentName: keyof JSXIdentifier | string;
+  /**
+   * An array of child SvgComponentDetails objects representing nested components.
+   */
+  children: SvgComponentDetails[] | [];
+  /**
+   * An object containing the props of the component, where the keys are the prop names
+   * and the values are the prop values.
+   */
+  props: {
+    [key: string]: any;
+  };
+}
