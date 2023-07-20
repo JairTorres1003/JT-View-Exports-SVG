de7abcce1479a9105cf03db3c653932ec450c98b
Jair Torres <83931760+JairTorres1003@users.noreply.github.com>
2023-07-20 20:18:43 +0000

    Add Search Engine

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Finder } from "./components/Finder";
 import { SvgExport } from "./interfaces/svgExports";
 import { vscode } from "./utilities/vscode";
 import SvgComponetExport from "./components/SvgComponetExport";
@@ -6,6 +7,10 @@ import SvgComponetExport from "./components/SvgComponetExport";
 function App() {
   const [svgComponents, setSvgComponents] = useState<SvgExport[]>([]);
 
+  /**
+   * Handle the SVG components data received from the webview.
+   * @param data The SVG components data.
+   */
   const handleSvgComponents = (data: any) => {
     setSvgComponents(JSON.parse(data));
   };
@@ -21,6 +26,7 @@ function App() {
 
   return (
     <main>
+      <Finder setSvgComponents={setSvgComponents} />
       {svgComponents.map((item, index) => (
         <SvgComponetExport {...item} key={index} />
       ))}
