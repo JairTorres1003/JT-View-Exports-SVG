135d6a401ac3dace7571fd3f64fef946460179c5
Jair Torres <83931760+JairTorres1003@users.noreply.github.com>
2023-07-20 20:21:33 +0000

    Change styles

--- webview-ui/src/components/SvgComponetExport.style.tsx
+++ webview-ui/src/components/SvgComponetExport.style.tsx
@@ -1,5 +1,56 @@
-import { Paper as PaperMui, Typography as TypographyMui, styled } from "@mui/material";
+import {
+  Accordion as AccordionMui,
+  AccordionSummary as AccordionSummaryMui,
+  Paper as PaperMui,
+  Typography as TypographyMui,
+  styled,
+  AccordionSummaryProps,
+  PaperProps,
+} from "@mui/material";
 import GridMui from "@mui/material/Unstable_Grid2";
+
+const ArrowForwardIcon = () => (
+  <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" width={17}>
+    <path d="M6.23 20.23 8 22l10-10L8 2 6.23 3.77 14.46 12z"></path>
+  </svg>
+);
+
+export const Accordion = styled(AccordionMui)(() => ({
+  "backgroundColor": "inherit",
+  "margin": "0 !important",
+  "&::before": {
+    display: "none",
+  },
+}));
+
+export const AccordionSummary = styled((props: AccordionSummaryProps | PaperProps) => (
+  <AccordionSummaryMui component={PaperMui} expandIcon={<ArrowForwardIcon />} {...props} />
+))(({ theme }) => ({
+  "flexDirection": "row-reverse",
+  "minHeight": "60px !important",
+  "maxHeight": 60,
+  "backgroundColor": "#fff",
+  "borderRadius": 4,
+  "transition": "background-color 0.2s",
+  "color": "#000",
+  "&.Mui-expanded": {
+    backgroundColor: "transparent",
+    borderBottom: "1px outset",
+    borderRadius: 0,
+    color: "inherit",
+  },
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
+    fill: "var(--color-JT-view-export-svg)",
+  },
+  "& .MuiAccordionSummary-content, & .MuiAccordionSummary-content.Mui-expanded": {
+    "margin": 0,
+    "marginLeft": theme.spacing(1),
+    "& .MuiTypography-root": {
+      fontSize: "16px",
+    },
+  },
+}));
 
 export const Grid = styled(GridMui)(() => ({
   display: "grid",
@@ -28,4 +79,5 @@ export const Paper = styled(PaperMui)(() => ({
 export const Typography = styled(TypographyMui)(() => ({
   fontSize: 12,
   textAlign: "center",
+  color: "inherit",
 }));
--- webview-ui/src/components/SvgComponetExport.tsx
+++ webview-ui/src/components/SvgComponetExport.tsx
@@ -1,26 +1,39 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import { SvgExport } from "../interfaces/svgExports";
 import RenderSVG from "./RenderSvg";
 import FailExport from "../icons/FailExport.svg";
-import { Grid, GridItem, Paper, Typography } from "./SvgComponetExport.style";
+import {
+  Accordion,
+  AccordionSummary,
+  Grid,
+  GridItem,
+  Paper,
+  Typography,
+} from "./SvgComponetExport.style";
+import { AccordionDetails } from "@mui/material";
 
 const SvgComponetExport: FunctionComponent<SvgExport> = (props) => {
   const { file, svgComponents } = props;
+  const [isExpanded, setIsExpanded] = useState<boolean>(true);
 
   return (
-    <div>
-      <h4>{file.relativePath}</h4>
-      <Grid container spacing={2}>
-        {svgComponents.map((item, index) => (
-          <GridItem key={index}>
-            <Paper elevation={3}>
-              {item.component ? <RenderSVG {...item.component} /> : <FailExport />}
-            </Paper>
-            <Typography noWrap>{item.name}</Typography>
-          </GridItem>
-        ))}
-      </Grid>
-    </div>
+    <Accordion elevation={0} expanded={isExpanded} onChange={() => setIsExpanded(!isExpanded)}>
+      <AccordionSummary elevation={isExpanded ? 0 : 3}>
+        <Typography noWrap>{file.relativePath}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Grid container spacing={2}>
+          {svgComponents.map((item, index) => (
+            <GridItem key={index}>
+              <Paper elevation={3}>
+                {item.component ? <RenderSVG {...item.component} /> : <FailExport />}
+              </Paper>
+              <Typography noWrap>{item.name}</Typography>
+            </GridItem>
+          ))}
+        </Grid>
+      </AccordionDetails>
+    </Accordion>
   );
 };
 
