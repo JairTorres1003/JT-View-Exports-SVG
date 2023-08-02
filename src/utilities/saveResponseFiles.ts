1c977e4a8b0b6a598c04d80de702788c2d76e8b5
Jair Torres <83931760+JairTorres1003@users.noreply.github.com>
2023-08-02 16:55:19 +0000

    Add save response - optional

---
+++ src/utilities/saveResponseFiles.ts
@@ -0,0 +1,18 @@
+import { window, workspace } from "vscode";
+import * as fs from "fs";
+
+export function saveResponseFile(data: object) {
+  const content = `import { SvgExport } from "../interfaces/svgExports";
+
+export const responseFile: SvgExport[] = ${JSON.stringify(data, null, 2)}`;
+
+  const filePath = workspace.rootPath + "/responseFile.json";
+
+  fs.writeFile(filePath, content, (err) => {
+    if (err) {
+      window.showErrorMessage("Error al crear y guardar el archivo.");
+    } else {
+      window.showInformationMessage(`Archivo guardado: ${filePath}.`);
+    }
+  });
+}
