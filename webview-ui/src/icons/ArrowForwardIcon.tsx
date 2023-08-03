8731c6e2eff2c6390a60b178ab1cfa1fa5f644f0
Jair Torres <83931760+JairTorres1003@users.noreply.github.com>
2023-08-02 22:58:34 -0500

    Change prop

--- webview-ui/src/icons/ArrowForwardIcon.tsx
+++ webview-ui/src/icons/ArrowForwardIcon.tsx
@@ -1,5 +1,10 @@
 export const ArrowForwardIcon = () => (
-  <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" width={17}>
+  <svg
+    focusable="false"
+    aria-hidden="true"
+    viewBox="0 0 24 24"
+    width={17}
+    xmlns="http://www.w3.org/2000/svg">
     <path d="M6.23 20.23 8 22l10-10L8 2 6.23 3.77 14.46 12z"></path>
   </svg>
 );
