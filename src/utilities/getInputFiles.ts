1a4bd01a1bf8625612c21e82b218fd1053d2eb73
Jair Torres <83931760+JairTorres1003@users.noreply.github.com>
2023-08-30 23:03:35 +0000

    use extractSVGComponentExports

--- src/utilities/getInputFiles.ts
+++ src/utilities/getInputFiles.ts
@@ -1,29 +1,39 @@
 import { ProgressLocation, ProgressOptions, window } from "vscode";
 import * as path from "path";
 
-import { SvgExportErrors, SvgFile } from "../interfaces/svgExports";
+import { SvgExport, SvgExportErrors, SvgFile } from "../interfaces/svgExports";
 import { getWorkspaceFolder } from "./getWorkspaceFolder";
 import { getTranslations } from "./getLocaleLanguage";
 import { REGEX_FILE } from "./regex";
+import { extractSVGComponentExports } from "./svg/exportParser";
+
 /**
  *
  * @param filesPath
  */
 export async function getInputFiles(filesPath: string[] | null) {
-  const i18n = getTranslations();
-  const selectedFiles: SvgFile[] = [];
-  const workspaceFolder: string = getWorkspaceFolder();
-  const newError: SvgExportErrors = { messageError: "" };
+  try {
+    const i18n = getTranslations();
+    const selectedFiles: SvgFile[] = [];
+    const workspaceFolder: string = getWorkspaceFolder();
+    const newError: SvgExportErrors = { messageError: "" };
+    const progressOptions: ProgressOptions = {
+      location: ProgressLocation.Notification,
+      title: i18n.progressTitle,
+      cancellable: false,
+    };
 
-  const progressOptions: ProgressOptions = {
-    location: ProgressLocation.Notification,
-    title: i18n.progressTitle,
-    cancellable: false,
-  };
+    // Show loader message
+    const progress = await window.withProgress(progressOptions, async (progress) => {
+      if (!filesPath || filesPath.length === 0) {
+        newError.messageError = i18n.NoFileSelected;
+        newError.fileSelected = 0;
 
-  // Show loader message
-  const progress: any = await window.withProgress(progressOptions, async () => {
-    if (filesPath && filesPath.length > 0) {
+        // Update the webview panel
+        // code update ..
+        return;
+      }
+
       filesPath.sort();
 
       filesPath.forEach((file) => {
@@ -33,24 +43,35 @@ export async function getInputFiles(filesPath: string[] | null) {
           selectedFiles.push({ absolutePath: file, relativePath });
         }
       });
-    } else {
-      newError.messageError = i18n.NoFileSelected;
-      newError.fileSelected = 0;
+
+      // Extract the exports from selected files
+      const svgComponents: SvgExport[] = await Promise.all(
+        selectedFiles.map(async (file) => {
+          try {
+            const svgExports = await extractSVGComponentExports(file.absolutePath);
+            return { ...svgExports, file, lengthSvg: svgExports.svgComponents.length };
+          } catch (error) {
+            console.error(`Error parsing file ${file.absolutePath}: ${error}`);
+            return { file, lengthExports: 0, lengthSvg: 0, svgComponents: [] };
+          }
+        })
+      );
+
+      if (svgComponents.length <= 0 && selectedFiles.length > 0) {
+        newError.messageError = i18n.NoIconsFound;
+        newError.fileSelected = selectedFiles.length;
+      }
 
       // Update the webview panel
       // code update ..
+      return progress;
+    });
 
-      return;
+    if (progress) {
+      // Hide the loader message
+      progress.report({ increment: 100 });
     }
-    console.log(selectedFiles);
-
-    // return svgComponents;
-    return;
-  });
-
-  if (progress) {
-    // Hide the loader message
-    progress.report({ increment: 100 });
-    progress.dispose();
+  } catch (error) {
+    console.error("An error occurred:", error);
   }
 }
