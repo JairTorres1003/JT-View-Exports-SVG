ec7d30504da7bdf5068a1ac226afc5ea22220eed
Jair Torres <83931760+JairTorres1003@users.noreply.github.com>
2023-09-06 01:05:48 +0000

    fix error provider

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -10,7 +10,6 @@ import { Loading } from "./components/Loading/Loading";
 
 import useApp from "./hooks/useApp";
 import PanelsSettings from "./components/Panels/PanelsSettings";
-import SvgProvider from "./provider/SvgProvider";
 
 function App() {
   const {
@@ -34,29 +33,29 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <SvgProvider>
-        <main>
-          {isLoading ? (
-            <Loading />
-          ) : fileSelected && fileSelected > 0 ? (
-            <Fragment>
-              <div className="BoxContainer">
-                <Resizable
-                  defaultSize={{ width: "100%", height: "100%" }}
-                  maxWidth="100%"
-                  minWidth="150"
-                  minHeight="100%"
-                  maxHeight="100%"
-                  enable={{ right: true }}
-                  onResize={handleResize}
-                  onResizeStop={handleResizeStop}
-                  size={{ width: resizableWidth, height: "100%" }}>
-                  <Box className="BoxContainer-gallery">
-                    <Finder
-                      isOpenPanel={isPanelOpen}
-                      handleSvgComponents={handleSvgComponents}
-                      refPortalButton={refPortalButton}
-                    />
+      <main>
+        {isLoading ? (
+          <Loading />
+        ) : fileSelected && fileSelected > 0 ? (
+          <Fragment>
+            <div className="BoxContainer">
+              <Resizable
+                defaultSize={{ width: "100%", height: "100%" }}
+                maxWidth="100%"
+                minWidth="150"
+                minHeight="100%"
+                maxHeight="100%"
+                enable={{ right: true }}
+                onResize={handleResize}
+                onResizeStop={handleResizeStop}
+                size={{ width: resizableWidth, height: "100%" }}>
+                <Box className="BoxContainer-gallery">
+                  <Finder
+                    isOpenPanel={isPanelOpen}
+                    handleSvgComponents={handleSvgComponents}
+                    refPortalButton={refPortalButton}
+                  />
+                  <Box className="BoxContainer-gallery-container">
                     {showMessage ? (
                       <Box>
                         <Typography variant="h1" fontSize={20}>
@@ -69,31 +68,31 @@ function App() {
                       ))
                     )}
                   </Box>
-                </Resizable>
-                <PanelsSettings
-                  isOpenPanel={isPanelOpen}
-                  handleOpenPanel={handleOpenPanel}
-                  refPortalButton={refPortalButton}
-                />
-              </div>
-              <Snackbar
-                key={snackbar.name}
-                open={snackbar.open}
-                autoHideDuration={2000}
-                onClose={() => setSnackbar({ open: false, name: "" })}
-                anchorOrigin={{ vertical: "bottom", horizontal: "center" }}>
-                <Alert severity="success">
-                  <Typography fontSize={12}>
-                    {`${t("Copied")} ${snackbar.name && `'${snackbar.name}' `}${t("ToClipboard")}`}
-                  </Typography>
-                </Alert>
-              </Snackbar>
-            </Fragment>
-          ) : (
-            <DropZone onExtractIcons={handleExtractIcons} />
-          )}
-        </main>
-      </SvgProvider>
+                </Box>
+              </Resizable>
+              <PanelsSettings
+                isOpenPanel={isPanelOpen}
+                handleOpenPanel={handleOpenPanel}
+                refPortalButton={refPortalButton}
+              />
+            </div>
+            <Snackbar
+              key={snackbar.name}
+              open={snackbar.open}
+              autoHideDuration={2000}
+              onClose={() => setSnackbar({ open: false, name: "" })}
+              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}>
+              <Alert severity="success">
+                <Typography fontSize={12}>
+                  {`${t("Copied")} ${snackbar.name && `'${snackbar.name}' `}${t("ToClipboard")}`}
+                </Typography>
+              </Alert>
+            </Snackbar>
+          </Fragment>
+        ) : (
+          <DropZone onExtractIcons={handleExtractIcons} />
+        )}
+      </main>
     </ThemeProvider>
   );
 }
