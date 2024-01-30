dccda1d37f1ee1d7cc99a14a297b9d5de3cf66e1
Jair Torres <jairalex0306.jatc@gmail.com>
2024-01-30 11:32:58 -0500

    Refactor IconClose component to accept size and color props

--- webview-ui/src/icons/index.tsx
+++ webview-ui/src/icons/index.tsx
@@ -13,15 +13,15 @@ export const ArrowForwardIcon = () => (
   </svg>
 )
 
-export const IconClose = () => {
+export const IconClose: FC<IconProps> = ({ size = '1rem', color = 'currentColor' }) => {
   return (
     <svg
-      stroke='currentColor'
-      fill='currentColor'
+      stroke={color}
+      fill={color}
       strokeWidth='0'
       viewBox='0 0 512 512'
-      height='1em'
-      width='1em'
+      height={size}
+      width={size}
       xmlns='http://www.w3.org/2000/svg'
     >
       <path d='M405 136.798L375.202 107 256 226.202 136.798 107 107 136.798 226.202 256 107 375.202 136.798 405 256 285.798 375.202 405 405 375.202 285.798 256z'></path>
