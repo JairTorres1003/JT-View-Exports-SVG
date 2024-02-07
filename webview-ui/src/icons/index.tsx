dad8b8d0ec4acc414401cdea02c7953655e08f2e
Jair Torres <jairalex0306.jatc@gmail.com>
2024-02-07 11:48:16 -0500

    Add sx prop to FolderIcon and FolderOpenIcon components

--- webview-ui/src/icons/index.tsx
+++ webview-ui/src/icons/index.tsx
@@ -1,10 +1,11 @@
 import { motion } from 'framer-motion'
-import { type FC } from 'react'
+import { CSSProperties, type FC } from 'react'
 
 interface IconProps {
   size?: number | string
   color?: string
   className?: string
+  sx?: CSSProperties
 }
 
 export const ArrowForwardIcon = () => (
@@ -524,7 +525,7 @@ export const IconFragmentCode: FC<IconProps> = ({ color = 'currentColor', size =
   </svg>
 )
 
-export const FolderIcon: FC<IconProps> = ({ color = 'currentColor', size = 24 }) => (
+export const FolderIcon: FC<IconProps> = ({ color = 'currentColor', size = 24, sx = {} }) => (
   <svg
     stroke={color}
     fill={color}
@@ -532,13 +533,14 @@ export const FolderIcon: FC<IconProps> = ({ color = 'currentColor', size = 24 })
     viewBox='0 0 16 16'
     height={size}
     width={size}
+    style={sx}
     xmlns='http://www.w3.org/2000/svg'
   >
     <path d='M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z'></path>
   </svg>
 )
 
-export const FolderOpenIcon: FC<IconProps> = ({ color = 'currentColor', size = 24 }) => (
+export const FolderOpenIcon: FC<IconProps> = ({ color = 'currentColor', size = 24, sx = {} }) => (
   <svg
     stroke={color}
     fill={color}
@@ -546,6 +548,7 @@ export const FolderOpenIcon: FC<IconProps> = ({ color = 'currentColor', size = 2
     viewBox='0 0 16 16'
     height={size}
     width={size}
+    style={sx}
     xmlns='http://www.w3.org/2000/svg'
   >
     <path d='M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v.64c.57.265.94.876.856 1.546l-.64 5.124A2.5 2.5 0 0 1 12.733 15H3.266a2.5 2.5 0 0 1-2.481-2.19l-.64-5.124A1.5 1.5 0 0 1 1 6.14V3.5zM2 6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5a.5.5 0 0 0-.5.5V6zm-.367 1a.5.5 0 0 0-.496.562l.64 5.124A1.5 1.5 0 0 0 3.266 14h9.468a1.5 1.5 0 0 0 1.489-1.314l.64-5.124A.5.5 0 0 0 14.367 7H1.633z'></path>
