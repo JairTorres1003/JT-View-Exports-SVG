52bbbd44ae83f0900a23af76d00109290e61f7ad
Jair Torres <jairalex0306.jatc@gmail.com>
2024-02-07 11:34:46 -0500

    Add AssetTable component to AssetFiles

---
+++ webview-ui/src/components/AssetFiles/AssetTable.tsx
@@ -0,0 +1,80 @@
+import {
+  Button,
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Tooltip,
+} from '@mui/material'
+import { useTranslation } from 'react-i18next'
+import MiddleEllipsis from 'react-middle-ellipsis'
+
+import { IconClose, IconFragmentCode } from '../../icons'
+import { type FC } from 'react'
+import { type AssetTableProps } from '../../interfaces/AssetTable'
+
+export const AssetTable: FC<AssetTableProps> = ({
+  paths,
+  handleOpenFile,
+  handleRemoveAsset,
+  handleViewAsset,
+}) => {
+  const { t } = useTranslation()
+
+  return (
+    <TableContainer component={Paper} className='assetFiles__table'>
+      <Table size='small'>
+        <TableBody>
+          {paths.map((path, index) => (
+            <TableRow key={index} className='assetFiles__tableRow'>
+              <TableCell component='th' scope='row' className='assetFiles__tableCell'>
+                <Button
+                  fullWidth
+                  className='assetFiles__tableCellButton'
+                  onClick={() => {
+                    handleViewAsset(path)
+                  }}
+                >
+                  <MiddleEllipsis>
+                    <Tooltip title={path} arrow>
+                      <span>{path}</span>
+                    </Tooltip>
+                  </MiddleEllipsis>
+                </Button>
+              </TableCell>
+              <TableCell align='right' className='assetFiles__tableCell' width={40}>
+                <IconButton
+                  size='small'
+                  color='success'
+                  title={t('OpenFile')}
+                  className='assetFiles__tableCellIconButton'
+                  onClick={() => {
+                    handleOpenFile(path)
+                  }}
+                >
+                  <IconFragmentCode size={18} />
+                </IconButton>
+              </TableCell>
+              <TableCell align='right' className='assetFiles__tableCell' width={40}>
+                <IconButton
+                  size='small'
+                  color='error'
+                  title={t('delete')}
+                  className='assetFiles__tableCellIconButton'
+                  onClick={() => {
+                    handleRemoveAsset(path)
+                  }}
+                >
+                  <IconClose size={18} color='red' />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
--- webview-ui/src/components/AssetFiles/index.tsx
+++ webview-ui/src/components/AssetFiles/index.tsx
@@ -1,82 +1,14 @@
-import {
-  Button,
-  IconButton,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableRow,
-  Tooltip,
-  Typography,
-} from '@mui/material'
+import { Typography } from '@mui/material'
 import { useTranslation } from 'react-i18next'
-import MiddleEllipsis from 'react-middle-ellipsis'
 
 import { AssetFilesBox } from './AssetFiles.style'
+import { AssetTable } from './AssetTable'
+
 import { useAssetFiles } from '../../hooks/useAssetFiles'
-import { IconClose, IconFragmentCode } from '../../icons'
-import { type FC } from 'react'
 
 const AssetFiles = () => {
-  const { assetFiles, handleOpenFile, handleRemoveAsset, handleViewAsset } = useAssetFiles()
+  const { assetFiles, ...useProps } = useAssetFiles()
   const { t } = useTranslation()
-
-  const AssetTable: FC<{ paths: string[] }> = ({ paths }) => {
-    return (
-      <TableContainer component={Paper} className='assetFiles__table'>
-        <Table size='small'>
-          <TableBody>
-            {paths.map((path, index) => (
-              <TableRow key={index} className='assetFiles__tableRow'>
-                <TableCell component='th' scope='row' className='assetFiles__tableCell'>
-                  <Button
-                    fullWidth
-                    className='assetFiles__tableCellButton'
-                    onClick={() => {
-                      handleViewAsset(path)
-                    }}
-                  >
-                    <MiddleEllipsis>
-                      <Tooltip title={path} arrow>
-                        <span>{path}</span>
-                      </Tooltip>
-                    </MiddleEllipsis>
-                  </Button>
-                </TableCell>
-                <TableCell align='right' className='assetFiles__tableCell' width={40}>
-                  <IconButton
-                    size='small'
-                    color='success'
-                    title={t('OpenFile')}
-                    className='assetFiles__tableCellIconButton'
-                    onClick={() => {
-                      handleOpenFile(path)
-                    }}
-                  >
-                    <IconFragmentCode size={18} />
-                  </IconButton>
-                </TableCell>
-                <TableCell align='right' className='assetFiles__tableCell' width={40}>
-                  <IconButton
-                    size='small'
-                    color='error'
-                    title={t('delete')}
-                    className='assetFiles__tableCellIconButton'
-                    onClick={() => {
-                      handleRemoveAsset(path)
-                    }}
-                  >
-                    <IconClose size={18} color='red' />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    )
-  }
 
   return (
     <AssetFilesBox>
@@ -84,7 +16,7 @@ const AssetFiles = () => {
         {t('AssetFiles.Title.Workspace')}
       </Typography>
       {assetFiles && assetFiles.workspace.length > 0 ? (
-        <AssetTable paths={assetFiles.workspace} />
+        <AssetTable paths={assetFiles.workspace} {...useProps} />
       ) : (
         <Typography variant='h6' component='h2' fontSize={16}>
           {t('AssetFiles.Empty.Workspace')}
@@ -95,7 +27,7 @@ const AssetFiles = () => {
           <Typography component='h1' fontSize={20}>
             {t('AssetFiles.Title.Others')}
           </Typography>
-          <AssetTable paths={assetFiles.user} />
+          <AssetTable paths={assetFiles.user} {...useProps} />
         </>
       )}
     </AssetFilesBox>
