b65f90cc25eb56dc2c899bf701f40753c2e707a5
JairTorres1003 <jairalex0306.jatc@gmail.com>
2024-06-27 11:01:49 -0500

    refactor: Improve AssetFiles component UI and functionality

--- webview-ui/src/components/AssetFiles/index.tsx
+++ webview-ui/src/components/AssetFiles/index.tsx
@@ -18,58 +18,58 @@ const AssetFiles = (): React.JSX.Element => {
   } = useAssetFiles()
   const { t } = useTranslation()
 
+  if (assetFiles === undefined) return <></>
+
   return (
     <AssetFilesBox>
-      <Box className='AssetFilesBox__actions'>
-        <Checkbox
-          onChange={handleCheckAllFiles}
-          title={t('selectAllFiles')}
-          inputProps={{ 'aria-label': 'select all files' }}
-          checked={
-            checkedFiles.length ===
-            (assetFiles?.workspace.length ?? 0) + (assetFiles?.user.length ?? 0)
-          }
-          indeterminate={
-            checkedFiles.length > 0 &&
-            checkedFiles.length <
-              (assetFiles?.workspace.length ?? 0) + (assetFiles?.user.length ?? 0)
-          }
-        />
-        <Box className='AssetFilesBox__actions__button'>
-          <Button
-            color='success'
-            variant='outlined'
-            size='small'
-            disabled={checkedFiles.length === 0}
-            onClick={handleViewAssets}
-          >
-            {t('OpenFiles')}
-          </Button>
-          <Button
-            color='error'
-            variant='outlined'
-            size='small'
-            disabled={checkedFiles.length === 0}
-            onClick={handleRemoveAssets}
-          >
-            {t('deleteFiles')}
-          </Button>
+      {[...assetFiles.user, ...assetFiles.workspace].length > 0 && (
+        <Box className='AssetFilesBox__actions'>
+          <Checkbox
+            onChange={handleCheckAllFiles}
+            title={t('selectAllFiles')}
+            inputProps={{ 'aria-label': 'select all files' }}
+            checked={checkedFiles.length === [...assetFiles.user, ...assetFiles.workspace].length}
+            indeterminate={
+              checkedFiles.length > 0 &&
+              checkedFiles.length < [...assetFiles.user, ...assetFiles.workspace].length
+            }
+          />
+          <Box className='AssetFilesBox__actions__button'>
+            <Button
+              color='success'
+              variant='outlined'
+              size='small'
+              disabled={checkedFiles.length === 0}
+              onClick={handleViewAssets}
+            >
+              {t('OpenFiles')}
+            </Button>
+            <Button
+              color='error'
+              variant='outlined'
+              size='small'
+              disabled={checkedFiles.length === 0}
+              onClick={handleRemoveAssets}
+            >
+              {t('deleteFiles')}
+            </Button>
+          </Box>
         </Box>
-      </Box>
+      )}
       <Typography component='h1' fontSize={20}>
         <span>
           <FolderOpenIcon size={20} sx={{ margin: '0 8px -2px 0' }} />
         </span>
         {t('AssetFiles.Title.Workspace')}
       </Typography>
-      {assetFiles !== undefined && assetFiles.workspace.length > 0 ? (
+      {assetFiles.workspace.length > 0 ? (
         <AssetTable paths={assetFiles.workspace} checkedFiles={checkedFiles} {...useProps} />
       ) : (
         <Typography variant='h6' component='h2' fontSize={16}>
           {t('AssetFiles.Empty.Workspace')}
         </Typography>
       )}
-      {assetFiles !== undefined && assetFiles.user.length > 0 && (
+      {assetFiles.user.length > 0 && (
         <>
           <Typography component='h1' fontSize={20}>
             <span>
