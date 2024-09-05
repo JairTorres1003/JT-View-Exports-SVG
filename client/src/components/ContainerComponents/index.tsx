9a67886c3a829b6e6fdfcd99a1cfaeb8c5f52289
JairTorres1003 <jairalex0306.jatc@gmail.com>
2024-09-05 10:31:38 -0500

    refactor: Remove unused code and update ContainerComponents to include accordion functionality

--- client/src/components/ContainerComponents/index.tsx
+++ client/src/components/ContainerComponents/index.tsx
@@ -1,27 +1,15 @@
-import { useState } from 'react'
 import MiddleEllipsis from 'react-middle-ellipsis'
 
 import { useSelector } from '@/providers/redux/store'
 import { Accordion, AccordionDetails, AccordionSummary, Tooltip, Typography } from '@mui/material'
 
 export const ContainerComponents = () => {
-  const [expanded, setExpanded] = useState<string | false>('panel1')
-
-  const handleChange = (panel: string) => (_: React.SyntheticEvent, isExpanded: boolean) => {
-    setExpanded(isExpanded ? panel : false)
-  }
-
   const { components } = useSelector((state) => state.svg)
 
   return (
     <div>
-      {components.map((item) => (
-        <Accordion
-          disableGutters
-          key={item.groupKind}
-          expanded={expanded === item.groupKind}
-          onChange={handleChange(item.groupKind)}
-        >
+      {components.map((item, index) => (
+        <Accordion disableGutters key={item.groupKind} defaultExpanded={index === 0}>
           <AccordionSummary
             id={`${item.groupKind}-header`}
             aria-controls={`${item.groupKind}-content`}
