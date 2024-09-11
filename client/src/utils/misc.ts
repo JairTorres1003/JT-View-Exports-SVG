10ba97cf22a95764dbb82b59b0617b170c8818eb
JairTorres1003 <jairalex0306.jatc@gmail.com>
2024-09-11 17:02:22 -0500

    refactor: Add copyToClipboard and getUnknownError functions to misc.ts

--- client/src/utils/misc.ts
+++ client/src/utils/misc.ts
@@ -24,3 +24,54 @@ export const isEmpty = (
 
   return false
 }
+
+/**
+ * Copies the provided text to the clipboard.
+ *
+ * @param text - The text to be copied to the clipboard.
+ */
+export const copyToClipboard = async (text: string): Promise<void> => {
+  await navigator.clipboard.writeText(text)
+}
+
+/**
+ * Returns the error message from an unknown error.
+ * @param error - The unknown error from which to retrieve the message.
+ * @returns The error message.
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const getUnknownError = (error: any): string => {
+  if (isEmpty(error)) return 'Unknown error'
+
+  if (typeof error === 'string') {
+    return error
+  }
+
+  if (Array.isArray(error) && error.length > 0) {
+    return Object.values(error[0] as string[])[0]
+  }
+
+  if (error instanceof Error || error instanceof TypeError) {
+    return error.message
+  }
+
+  if ('data' in error) {
+    const message = error.data.message
+    if (typeof message === 'string') {
+      return message
+    } else if (Array.isArray(message) && message.length > 0) {
+      return Object.values(message[0] as string[])[0]
+    }
+  }
+
+  if ('message' in error) {
+    const message = error.message
+    if (typeof message === 'string') {
+      return message
+    } else if (Array.isArray(message) && message.length > 0) {
+      return Object.values(message[0] as string[])[0]
+    }
+  }
+
+  return 'Unknown error'
+}
