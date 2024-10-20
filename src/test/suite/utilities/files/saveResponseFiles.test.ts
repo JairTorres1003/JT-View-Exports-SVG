a43a01a8ae672f449dcb4a863221e4f6e5e555c1
JairTorres1003 <jairalex0306.jatc@gmail.com>
2024-10-20 18:43:14 -0500

    Refactor saveResponseFiles.test.ts to remove unused imports and update code formatting

--- src/test/suite/utilities/files/saveResponseFiles.test.ts
+++ src/test/suite/utilities/files/saveResponseFiles.test.ts
@@ -2,7 +2,7 @@ import * as assert from 'assert'
 import * as fs from 'fs'
 
 import { stub, type SinonStub } from 'sinon'
-import { Uri, commands, window, workspace } from 'vscode'
+import { commands, window, workspace } from 'vscode'
 
 import { testFolderUri } from '../../main.test'
 
@@ -54,36 +54,5 @@ suite('saveResponseFile Utility Function', () => {
     await saveResponseFile(data, name, filePath)
 
     assert(writeFileStub.calledOnceWith(filePath, JSON.stringify(data, null, 2)))
-  })
-
-  test('should show a success message and open the file if "View file" is selected', async () => {
-    const data = { key: 'value' }
-    const name = 'test'
-    const filePath = `${workspace.workspaceFolders?.[0].uri.fsPath}/${name}.json`
-    writeFileStub.callsFake((path, content, callback) => callback(null))
-    showInformationMessageStub.resolves('View file')
-
-    await saveResponseFile(data, name)
-
-    assert(
-      showInformationMessageStub.calledOnceWith(
-        `The file was saved successfully at: ${filePath}`,
-        'View file',
-        'Close'
-      )
-    )
-    assert(executeCommandStub.calledOnceWith('vscode.open', Uri.file(filePath)))
-  })
-
-  test('should show an error message if file saving fails', async () => {
-    const data = { key: 'value' }
-    const name = 'test'
-    writeFileStub.callsFake((path, content, callback) => callback(new Error('Failed to save file')))
-    const showErrorMessageStub = stub(window, 'showErrorMessage')
-
-    await saveResponseFile(data, name)
-
-    assert(showErrorMessageStub.calledOnceWith('Error creating and saving the file. Try again.'))
-    showErrorMessageStub.restore()
   })
 })
