1c2002158a0e5a2ee19bdffabd3f0151b17ba048
JairTorres1003 <jairalex0306.jatc@gmail.com>
2025-01-15 18:15:15 -0500

    Add error handling and async support to get-components route for SVG files

--- server/src/routes/svg.route.ts
+++ server/src/routes/svg.route.ts
@@ -2,33 +2,46 @@
 import { Router } from 'express'
 import * as path from 'path'
 
+import { pathToSVGFile } from '@jt/view-exports-svg/utilities/files/misc.js'
+import { svgFileToUri } from '@jt/view-exports-svg/utilities/vscode/uri.js'
+
 import { REGEX_FILE } from '@/constants/regex.ts'
 
 const router = Router()
 const BASE_PATH = Deno.env.get('BASE_PATH') ?? ''
 
-router.get('/get-components', function (_, res) {
-  const examplesPath = path.join(BASE_PATH, '..', 'examples')
+router.get('/get-components', async function (_, res) {
+  try {
+    const examplesPath = path.join(BASE_PATH, '..', 'examples')
 
-  const getAllFiles = (dirPath: string, arrayOfFiles: string[] = []): string[] => {
-    const files = Deno.readDirSync(dirPath)
+    const getAllFiles = (dirPath: string, arrayOfFiles: string[] = []): string[] => {
+      const files = Deno.readDirSync(dirPath)
 
-    for (const dirEntry of files) {
-      if (dirEntry.isDirectory) {
-        arrayOfFiles = getAllFiles(path.join(dirPath, dirEntry.name), arrayOfFiles)
-      } else {
-        arrayOfFiles.push(path.join(dirPath, dirEntry.name))
+      for (const dirEntry of files) {
+        if (dirEntry.isDirectory) {
+          arrayOfFiles = getAllFiles(path.join(dirPath, dirEntry.name), arrayOfFiles)
+        } else {
+          arrayOfFiles.push(path.join(dirPath, dirEntry.name))
+        }
       }
+
+      return arrayOfFiles
     }
 
-    return arrayOfFiles
+    const allFiles = getAllFiles(path.join(examplesPath, 'typescript'))
+      .concat(getAllFiles(path.join(examplesPath, 'javascript')))
+      .filter((file) => REGEX_FILE.test(file))
+      .map(pathToSVGFile)
+
+    const svgFiles = await Promise.all(allFiles)
+
+    const resolvedFiles = await Promise.all(svgFiles.map(svgFileToUri))
+
+    res.send(resolvedFiles)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send([])
   }
-
-  const allFiles = getAllFiles(path.join(examplesPath, 'typescript'))
-    .concat(getAllFiles(path.join(examplesPath, 'javascript')))
-    .filter((file) => REGEX_FILE.test(file))
-
-  res.send(allFiles)
 })
 
 export const svgRoutes = Router().use('/svg', router)
