4eaab397925fc9acb3ea775f9ea04cb768202b17
JairTorres1003 <jairalex0306.jatc@gmail.com>
2025-01-20 11:56:44 -0500

    Add error display for SVG components in ContainerComponents

--- client/src/components/ContainerComponents/index.style.tsx
+++ client/src/components/ContainerComponents/index.style.tsx
@@ -1,13 +1,25 @@
-import { Box, styled } from '@mui/material'
+import { Box, listItemIconClasses, styled } from '@mui/material'
 
-export const BoxContainerComponents = styled(Box)(() => ({
+export const BoxContainerComponents = styled(Box)(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
+  height: '100%',
+  overflowY: 'auto',
   '& .BoxContainerComponents__accordion-details': {
     display: 'grid',
     gridAutoRows: 'auto',
     gridAutoColumns: 'max-content',
     gridTemplateColumns: 'repeat(auto-fill, minmax(100px, 1fr))',
     gap: '12px',
+  },
+  '& .BoxContainerComponents__error': {
+    padding: '8px 16px',
+    minHeight: '38px',
+    display: 'flex',
+    gap: '4px',
+    [`& .${listItemIconClasses.root}`]: {
+      height: theme.typography.fontSize + 2,
+      minWidth: 24,
+    },
   },
 }))
--- client/src/components/ContainerComponents/index.tsx
+++ client/src/components/ContainerComponents/index.tsx
@@ -1,4 +1,13 @@
-import { Accordion, AccordionDetails, AccordionSummary, Tooltip, Typography } from '@mui/material'
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Tooltip,
+  Typography,
+} from '@mui/material'
 import MiddleEllipsis from 'react-middle-ellipsis'
 
 import { CardSvg } from '../Cards'
@@ -6,12 +15,26 @@ import RenderSvg from '../SVG/RenderSvg'
 
 import { BoxContainerComponents } from './index.style'
 
+import { IconWarning } from '@/assets/icons/indicators'
 import { useContainerComponents } from '@/hooks/components/useContainerComponents'
 import { useSelector } from '@/providers/redux/store'
 
 export const ContainerComponents = (): React.ReactNode => {
   const { isExpanded, toggleExpanded } = useContainerComponents()
-  const { components } = useSelector((state) => state.svg)
+  const { components, errors } = useSelector((state) => state.svg)
+
+  if (errors !== undefined) {
+    return (
+      <BoxContainerComponents>
+        <ListItem component='div' className='BoxContainerComponents__error'>
+          <ListItemIcon>
+            <IconWarning size='100%' />
+          </ListItemIcon>
+          <ListItemText primary={errors.message} />
+        </ListItem>
+      </BoxContainerComponents>
+    )
+  }
 
   return (
     <BoxContainerComponents>
