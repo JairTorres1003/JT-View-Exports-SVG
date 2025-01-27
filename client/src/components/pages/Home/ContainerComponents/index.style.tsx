7a28b619011ff5384f84f07507bb081b134e9056
JairTorres1003 <jairalex0306.jatc@gmail.com>
2025-01-27 17:03:32 -0500

    Add ContainerComponents with error handling and accordion functionality; refactor styles

--- client/src/components/pages/Home/ContainerComponents/index.style.tsx
+++ client/src/components/pages/Home/ContainerComponents/index.style.tsx
@@ -1,18 +1,20 @@
 import { Box, listItemIconClasses, styled } from '@mui/material'
 
-export const BoxContainerComponents = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  height: '100%',
-  overflowY: 'auto',
-  '& .BoxContainerComponents__error': {
-    padding: '8px 16px',
-    minHeight: '38px',
+export const BoxContainerComponents = styled(Box, { name: 'BoxContainerComponents' })(
+  ({ theme }) => ({
     display: 'flex',
-    gap: '4px',
-    [`& .${listItemIconClasses.root}`]: {
-      height: theme.typography.fontSize + 2,
-      minWidth: 24,
+    flexDirection: 'column',
+    height: '100%',
+    overflowY: 'auto',
+    '& .BoxContainerComponents__error': {
+      padding: '8px 16px',
+      minHeight: '38px',
+      display: 'flex',
+      gap: '4px',
+      [`& .${listItemIconClasses.root}`]: {
+        height: theme.typography.fontSize + 2,
+        minWidth: 24,
+      },
     },
-  },
-}))
+  })
+)
