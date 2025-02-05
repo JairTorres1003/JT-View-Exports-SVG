8e1c21142f0b196655e0f46b0188a3b0bb244f0c
JairTorres1003 <jairalex0306.jatc@gmail.com>
2025-02-05 18:08:16 -0500

    Refactor CardSvg styles to use theme variables for color, border, and background for improved consistency

--- client/src/modules/Home/components/Cards/CardSvg/CardSvg.style.tsx
+++ client/src/modules/Home/components/Cards/CardSvg/CardSvg.style.tsx
@@ -1,24 +1,28 @@
 import { Box, styled } from '@mui/material'
 
-export const BoxCardSvg = styled(Box, { name: 'Box-CardSvg', slot: 'Root' })(() => ({
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'flex-start',
-  gap: '4px',
-  cursor: 'pointer',
-  '& .Box-CardSvg__card': {
-    padding: '10px',
-    display: 'grid',
-    placeContent: 'center',
-    height: '66px',
-    position: 'relative',
-    color: 'var(--JT-SVG-vscode-input-background, currentColor)',
-    '& > :first-of-type': {
-      width: 'calc(100% - 30px) !important',
-      height: 'calc(100% - 34px) !important',
-      position: 'absolute !important',
-      inset: '0 !important',
-      margin: 'auto !important',
+export const BoxCardSvg = styled(Box, { name: 'Box-CardSvg', slot: 'Root' })(
+  ({ theme: { vars } }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    gap: '4px',
+    cursor: 'pointer',
+    '& .Box-CardSvg__card': {
+      padding: '10px',
+      display: 'grid',
+      placeContent: 'center',
+      height: '66px',
+      position: 'relative',
+      color: `var(${vars.palette.text.primary}, currentColor)`,
+      border: `1px solid var(--JT-SVG-vscode-sideBarSectionHeader-border)`,
+      backgroundColor: `var(--JT-SVG-vscode-sideBarSectionHeader-background)`,
+      '& > :first-of-type': {
+        width: 'calc(100% - 30px) !important',
+        height: 'calc(100% - 34px) !important',
+        position: 'absolute !important',
+        inset: '0 !important',
+        margin: 'auto !important',
+      },
     },
-  },
-}))
+  })
+)
