215be2eecce9c5da46b6a8ac92ec46e14ea395c0
JairTorres1003 <jairalex0306.jatc@gmail.com>
2025-02-06 17:14:49 -0500

    Refactor AccordionMenuItem to use forwardRef for improved ref handling and extend slotProps with summary support

--- client/src/core/components/Accordion/AccordionMenuItem.tsx
+++ client/src/core/components/Accordion/AccordionMenuItem.tsx
@@ -7,41 +7,45 @@ import {
   Typography,
   type AccordionProps,
   type TooltipProps,
+  type AccordionSummaryProps,
 } from '@mui/material'
-import { useId, type FC } from 'react'
+import { forwardRef, useId } from 'react'
 
 export interface AccordionMenuItemProps extends AccordionProps {
   label: string
   slotProps?: AccordionProps['slotProps'] & {
     details?: Partial<Omit<AccordionDetailsProps, 'children'>>
     tooltip?: Partial<Omit<TooltipProps, 'title'>>
+    summary?: Partial<Omit<AccordionSummaryProps, 'children'>>
   }
 }
 
-export const AccordionMenuItem: FC<AccordionMenuItemProps> = ({
-  children,
-  label,
-  slotProps = {},
-  ...props
-}) => {
-  const id = useId()
+export const AccordionMenuItem = forwardRef<HTMLDivElement, AccordionMenuItemProps>(
+  function AccordionMenuItem({ children, label, slotProps = {}, ...props }, ref) {
+    const id = useId()
 
-  return (
-    <Accordion
-      {...props}
-      slotProps={{
-        heading: slotProps.heading,
-        transition: slotProps.transition,
-      }}
-    >
-      <AccordionSummary id={`${id}header`} aria-controls={`${id}content`}>
-        <Tooltip placement='top' {...(slotProps.tooltip ?? {})} title={label}>
-          <Typography component='span' noWrap>
-            {label}
-          </Typography>
-        </Tooltip>
-      </AccordionSummary>
-      <AccordionDetails {...(slotProps.details ?? {})}>{children}</AccordionDetails>
-    </Accordion>
-  )
-}
+    return (
+      <Accordion
+        {...props}
+        ref={ref}
+        slotProps={{
+          heading: slotProps.heading,
+          transition: slotProps.transition,
+        }}
+      >
+        <AccordionSummary
+          id={`${id}header`}
+          aria-controls={`${id}content`}
+          {...(slotProps.summary ?? {})}
+        >
+          <Tooltip placement='top' {...(slotProps.tooltip ?? {})} title={label}>
+            <Typography component='span' noWrap>
+              {label}
+            </Typography>
+          </Tooltip>
+        </AccordionSummary>
+        <AccordionDetails {...(slotProps.details ?? {})}>{children}</AccordionDetails>
+      </Accordion>
+    )
+  }
+)
