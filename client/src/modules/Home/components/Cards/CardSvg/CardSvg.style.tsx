7798e6a86b0d20401e352c7b546dab67ac242495
JairTorres1003 <jairalex0306.jatc@gmail.com>
2025-02-06 10:03:44 -0500

    Enhance CardSvg styles by adding a background overlay and adjusting z-index for improved visual hierarchy

--- client/src/modules/Home/components/Cards/CardSvg/CardSvg.style.tsx
+++ client/src/modules/Home/components/Cards/CardSvg/CardSvg.style.tsx
@@ -16,12 +16,22 @@ export const BoxCardSvg = styled(Box, { name: 'Box-CardSvg', slot: 'Root' })(
       color: `var(${vars.palette.text.primary}, currentColor)`,
       border: `1px solid var(--JT-SVG-vscode-sideBarSectionHeader-border)`,
       backgroundColor: `var(--JT-SVG-vscode-sideBarSectionHeader-background)`,
+      '&:before': {
+        content: '""',
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        background: 'rgb(255 255 255 / 2%)',
+        inset: 0,
+        zIndex: 0,
+      },
       '& > :first-of-type': {
         width: 'calc(100% - 30px) !important',
         height: 'calc(100% - 34px) !important',
         position: 'absolute !important',
         inset: '0 !important',
         margin: 'auto !important',
+        zIndex: 1,
       },
     },
   })
