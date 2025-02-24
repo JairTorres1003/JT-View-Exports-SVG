4f1c38d66b448c9d37f346d15f0cb4532b09e761
JairTorres1003 <jairalex0306.jatc@gmail.com>
2025-02-24 15:57:03 -0500

    Enhance Vite configuration with local dependency inclusion, improved build settings, and server options

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs'
 import { defineConfig } from 'vite'
 import importMetaUrlPlugin from '@codingame/esbuild-import-meta-url-plugin'
 import react from '@vitejs/plugin-react-swc'
@@ -5,31 +6,66 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { generateAssetName, generateFileName, generateWorkerFileName } from './vite.generateFiles'
 
+const pkg = JSON.parse(
+  fs.readFileSync(new URL('./package.json', import.meta.url).pathname).toString()
+)
+
+const localDependencies = Object.entries(pkg.dependencies as Record<string, string>)
+  .filter(([name]) => name.startsWith('@codingame/'))
+  .map(([name]) => name)
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
   optimizeDeps: {
     esbuildOptions: {
+      tsconfig: './tsconfig.app.json',
       plugins: [importMetaUrlPlugin],
     },
+    exclude: [],
+    include: [
+      ...localDependencies,
+      'vscode-textmate',
+      'vscode-oniguruma',
+      '@vscode/vscode-languagedetection',
+      'marked',
+    ],
   },
   build: {
+    target: 'esnext',
+    manifest: 'manifest.json',
+    chunkSizeWarningLimit: 1024,
     rollupOptions: {
       output: {
         chunkFileNames: generateFileName,
         assetFileNames: generateAssetName,
       },
     },
-    chunkSizeWarningLimit: 1024,
-    manifest: true,
+  },
+  esbuild: {
+    minifySyntax: false,
   },
   worker: {
+    format: 'es',
     rollupOptions: {
       output: {
         entryFileNames: generateWorkerFileName,
         chunkFileNames: 'assets/workers/[name]-[hash].js',
         assetFileNames: 'assets/workers/[name]-[hash].[ext]',
       },
+    },
+  },
+  define: {
+    rootDirectory: JSON.stringify(__dirname),
+  },
+  resolve: {
+    dedupe: ['vscode', 'monaco-editor', ...localDependencies],
+  },
+  server: {
+    port: 5173,
+    host: '0.0.0.0',
+    fs: {
+      allow: ['../'], // allow to load codicon.ttf from monaco-editor in the parent folder
     },
   },
 })
