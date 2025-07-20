b85b29f42f673cf244b87771b4e973775a6f857a
JairTorres1003 <jairalex0306.jatc@gmail.com>
2025-07-19 21:02:19 -0500

    chore: update dependencies and improve linting commands

--- client/src/config/environment.ts
+++ client/src/config/environment.ts
@@ -7,7 +7,7 @@ const configSchema = z.object({
   /**
    * The URL of the API that the client will use to communicate with the server.
    */
-  VITE_VSCODE_API_URL: z.string().url(),
+  VITE_VSCODE_API_URL: z.url(),
 })
 
 configSchema.parse(import.meta.env)
