278b1ba10e20c8544ae697e00bcf337ec79adb0e
JairTorres1003 <jairalex0306.jatc@gmail.com>
2025-07-24 23:11:37 -0500

    feat: add support for opening multiple files with new icons and labels

--- client/src/modules/dashboard/components/ActionsAccordion/ActionsAccordion.tsx
+++ client/src/modules/dashboard/components/ActionsAccordion/ActionsAccordion.tsx
@@ -3,7 +3,7 @@ import { IconButton, Stack, Tooltip } from '@mui/material'
 import { type FC, memo } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { IconGoToFile } from '@/assets/icons/functionalities'
+import { IconGoToFile, IconGoToManyFiles } from '@/assets/icons/functionalities'
 import { IconInfo } from '@/assets/icons/indicators'
 import { openFileInPosition } from '@/core/utils/file'
 
@@ -13,14 +13,16 @@ const ActionsAccordion: FC<{ data: ViewExportSVG }> = ({ data }) => {
   return (
     <Stack direction='row' spacing={0.5} alignItems='center'>
       <Tooltip
-        title={t('labels.OpenFile')}
+        title={t(`labels.${data.files.length > 1 ? 'OpenManyFiles' : 'OpenFile'}`)}
         placement='top'
         onClick={() => {
           openFileInPosition(data.files[0])
         }}
       >
-        <IconButton aria-label={t('labels.OpenFile')}>
-          <IconGoToFile size={16} />
+        <IconButton
+          aria-label={t(`labels.${data.files.length > 1 ? 'OpenManyFiles' : 'OpenFile'}`)}
+        >
+          {data.files.length > 1 ? <IconGoToManyFiles size={16} /> : <IconGoToFile size={16} />}
         </IconButton>
       </Tooltip>
       <Tooltip title={t('labels.Info')} placement='top'>
