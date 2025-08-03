0f3f3a9f88998d56130c64f810c2f82e3e510813
JairTorres1003 <jairalex0306.jatc@gmail.com>
2025-08-02 21:54:02 -0500

    fix: update context menu event to allow interaction with card SVG elements

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import Layout from './app/Layouts/main'
+import { cardSvgClasses } from './core/components/Cards/CardSvg'
 import i18next from './i18n'
 
 import './config/environment'
@@ -15,7 +16,7 @@ if (!container) {
 }
 
 document.addEventListener('contextmenu', (e) => {
-  const allowedElement = (e.target as HTMLElement).closest('input')
+  const allowedElement = (e.target as HTMLElement).closest(`input, .${cardSvgClasses.root}`)
   if (!allowedElement) {
     e.preventDefault()
   }
