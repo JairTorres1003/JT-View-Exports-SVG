ea82c9f6a3dccb203e2989ab77a07de499ac65d9
JairTorres1003 <jairalex0306.jatc@gmail.com>
2025-08-07 18:10:35 -0500

    Add localization support for multiple languages including Portuguese, Russian, Turkish, Chinese (Simplified and Traditional)
    - Added Portuguese (pt-BR) localization file with error messages, labels, and DevTools translations.
    - Added Russian (ru) localization file with error messages, labels, and DevTools translations.
    - Added Turkish (tr) localization file with error messages, labels, and DevTools translations.
    - Added Chinese (Simplified) (zh-CN) localization file with error messages, labels, and DevTools translations.
    - Added Chinese (Traditional) (zh-TW) localization file with error messages, labels, and DevTools translations.
    - Updated i18n configuration to support new languages and utilize language detection and backend loading.

--- client/src/i18n/index.ts
+++ client/src/i18n/index.ts
@@ -1,51 +1,42 @@
 import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import Backend from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
-
-import cs from './cs.json'
-import de from './de.json'
-import en from './en.json'
-import es from './es.json'
-import fr from './fr.json'
-import it from './it.json'
-import ja from './ja.json'
-import ko from './ko.json'
-import pl from './pl.json'
-import pt_br from './pt-br.json'
-import ru from './ru.json'
-import tr from './tr.json'
-import zh_cn from './zh-cn.json'
-import zh_tw from './zh-tw.json'
 
 /**
  * Initializes the i18n library with the specified configuration.
  * @returns The initialized i18n instance.
  */
 i18n
+  .use(Backend)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: {
-      cs: { translation: cs },
-      de: { translation: de },
-      en: { translation: en },
-      es: { translation: es },
-      fr: { translation: fr },
-      it: { translation: it },
-      ja: { translation: ja },
-      ko: { translation: ko },
-      pl: { translation: pl },
-      pt_br: { translation: pt_br },
-      ru: { translation: ru },
-      tr: { translation: tr },
-      zh_cn: { translation: zh_cn },
-      zh_tw: { translation: zh_tw },
-    },
-    lng: 'en',
     fallbackLng: 'en',
+    supportedLngs: [
+      'cs',
+      'de',
+      'en',
+      'es',
+      'fr',
+      'it',
+      'ja',
+      'ko',
+      'pl',
+      'pt-BR',
+      'ru',
+      'tr',
+      'zh-CN',
+      'zh-TW',
+    ],
     interpolation: {
       escapeValue: false,
     },
     react: {
       transKeepBasicHtmlNodesFor: ['b', 'strong', 'i'],
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}.json',
     },
   })
   .catch((err) => {
