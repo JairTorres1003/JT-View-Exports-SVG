9e8acdaf2425333b2a3775a29e9c08b4a1ee15d5
JairTorres1003 <jairalex0306.jatc@gmail.com>
2025-08-08 18:52:00 -0500

    fix: update launch configuration and package versions for improved build process

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,34 +1,12 @@
 const esbuild = require('esbuild')
+const polyfillGlobals = require('@esbuild-plugins/node-globals-polyfill')
+const polyfillModules = require('@esbuild-plugins/node-modules-polyfill')
 
 const production = process.argv.includes('--production')
 const watch = process.argv.includes('--watch')
+const webMode = process.argv.includes('--web-mode')
 
-async function main() {
-  const ctx = await esbuild.context({
-    entryPoints: ['src/extension.ts'],
-    bundle: true,
-    format: 'cjs',
-    minify: production,
-    sourcemap: !production,
-    sourcesContent: false,
-    platform: 'node',
-    outfile: 'out/cjs/extension.js',
-    external: ['vscode'],
-    logLevel: 'warning',
-    tsconfig: './tsconfig.cjs.json',
-    plugins: [esbuildProblemMatcherPlugin],
-  })
-  if (watch) {
-    await ctx.watch()
-  } else {
-    await ctx.rebuild()
-    await ctx.dispose()
-  }
-}
-
-/**
- * @type {import('esbuild').Plugin}
- */
+/** @type {import('esbuild').Plugin} */
 const esbuildProblemMatcherPlugin = {
   name: 'esbuild-problem-matcher',
 
@@ -45,6 +23,54 @@ const esbuildProblemMatcherPlugin = {
       console.log('[watch] build finished')
     })
   },
+}
+
+/** @type {import('esbuild').BuildOptions} */
+const options = {
+  platform: 'node',
+  outfile: 'out/cjs/extension.js',
+  plugins: [esbuildProblemMatcherPlugin],
+}
+
+/** @type {import('esbuild').BuildOptions} */
+const webOptions = {
+  platform: 'browser',
+  outfile: 'out/cjs/web/extension.js',
+  define: {
+    global: 'globalThis',
+  },
+  plugins: [
+    polyfillModules.NodeModulesPolyfillPlugin(),
+    polyfillGlobals.NodeGlobalsPolyfillPlugin({
+      process: true,
+      buffer: true,
+    }),
+    esbuildProblemMatcherPlugin,
+  ],
+}
+
+async function main() {
+  const ctx = await esbuild.context({
+    entryPoints: ['src/extension.ts'],
+    bundle: true,
+    format: 'cjs',
+    minify: production,
+    sourcemap: !production,
+    sourcesContent: false,
+    external: ['vscode'],
+    logLevel: 'warning',
+    tsconfig: './tsconfig.cjs.json',
+    ...(webMode ? webOptions : options),
+  })
+
+  if (watch) {
+    await ctx.watch()
+    console.log('[watch] Watching for changes...')
+  } else {
+    await ctx.rebuild()
+    await ctx.dispose()
+    console.log('[build] Web build complete.')
+  }
 }
 
 main().catch((e) => {
