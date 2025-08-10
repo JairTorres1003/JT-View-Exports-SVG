50db75d60ae440c7545d94b72ad241dfbc9073a0
JairTorres1003 <jairalex0306.jatc@gmail.com>
2025-08-10 14:27:03 -0500

    refactor: streamline copy actions in useContextMenuSVG hook

--- client/src/core/hooks/useContextMenuSVG.tsx
+++ client/src/core/hooks/useContextMenuSVG.tsx
@@ -1,39 +1,45 @@
 import type { SVGComponent } from '@api/types/ViewExportsSVG'
 import { Trans, useTranslation } from 'react-i18next'
 
+import { copyToClipboard, getUnknownError } from '@/utils/misc'
+
 import { useAlert } from './useAlert'
 import { useSVGActions, type UseSVGActionsReturn, type UseSVGActionsProps } from './useSVGActions'
-
-import { copyToClipboard, getUnknownError } from '@/utils/misc'
 
 export const useContextMenuSVG = (options: UseSVGActionsProps): UseSVGActionsReturn => {
   const { onOpen } = useAlert()
   const actions = useSVGActions(options)
   const { t } = useTranslation(undefined, { keyPrefix: 'labels' })
 
-  const onAlert = (component: SVGComponent) => {
-    onOpen(
-      <Trans t={t} i18nKey='Copied {{value}} to clipboard' values={{ value: component.name }} />,
-      { severity: 'success' }
-    )
-  }
-
-  /**
-   * Handles the copy action for an SVG component's name.
-   * Copies the component's name to the clipboard and triggers an alert on success.
-   * If an error occurs during the copy operation, displays an error alert.
-   *
-   * @param component - The SVG component whose name will be copied to the clipboard.
-   */
-  const onCopyName = (component: SVGComponent) => {
-    copyToClipboard(component.name)
+  const onCopy = (component: SVGComponent, value: string) => {
+    copyToClipboard(value)
       .then(() => {
-        onAlert(component)
+        onOpen(
+          <Trans
+            t={t}
+            i18nKey='Copied {{value}} to clipboard'
+            values={{ value: component.name }}
+          />,
+          { severity: 'success' }
+        )
       })
       .catch((error) => {
         onOpen(getUnknownError(error), { severity: 'error' })
       })
   }
 
-  return [{ label: t('CopyName'), onClick: onCopyName }, { isDivider: true }, ...actions]
+  const onCopyName = (component: SVGComponent) => {
+    onCopy(component, component.name)
+  }
+
+  const onCopyFragment = (component: SVGComponent) => {
+    onCopy(component, `<${component.name} />`)
+  }
+
+  return [
+    { label: t('CopyName'), onClick: onCopyName },
+    { label: t('CopyFragment'), onClick: onCopyFragment },
+    { isDivider: true },
+    ...actions,
+  ]
 }
