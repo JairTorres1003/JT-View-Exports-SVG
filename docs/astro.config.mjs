b950232efd7a9f8f5c544e92cf12d92d1883da29
JairTorres1003 <jairalex0306.jatc@gmail.com>
2025-11-11 12:33:17 -0500

    Implement code changes to enhance functionality and improve performance

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
   integrations: [
     starlight({
       plugins: [starlightImageZoom(), starlightThemeNova(), starlightChangelogs()],
-      favicon: '/favicon.svg',
+      favicon: 'favicon.ico',
       logo: {
         src: './src/assets/JT View Exports SVG - ICON.svg',
         alt: 'View Exports SVG Logo',
