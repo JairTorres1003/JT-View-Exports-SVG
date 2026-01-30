f6dd1a9f4ab22479a7d36493229e5be47bf91e58
JairTorres1003 <jairalex0306.jatc@gmail.com>
2026-01-30 16:31:14 -0500

    fix: update production URL in docusaurus configuration

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://jt-view-exports-svg.vercel.app',
+  url: 'https://view-exports-svg.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
