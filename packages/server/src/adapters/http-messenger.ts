96254b03206c0e011ce39c25906feb31712cdb18
JairTorres1003 <jairalex0306.jatc@gmail.com>
2026-03-16 11:03:35 -0500

    fix: add check for headersSent in HttpMessenger to prevent sending response multiple times

--- packages/server/src/adapters/http-messenger.ts
+++ packages/server/src/adapters/http-messenger.ts
@@ -5,6 +5,11 @@ export class HttpMessenger {
   constructor(private readonly res: Response) {}
 
   public postMessage: PostMessageEmitter = (type, data) => {
+    if (this.res.headersSent) {
+      console.warn('Response already sent, cannot post message:', { type, data })
+      return
+    }
+
     this.res.status(200).json({ type, data }).send()
   }
 }
